b3d6193da9b20126fcb402f48ed7bdb3e691788b
Jack Luo <jack.luo@mail.utoronto.ca>
2025-10-23 10:25:49 -0400

    refactor: Resolve minor lint issue in reader_parser.pyi

--- src/log_surgeon/reader_parser.pyi
+++ src/log_surgeon/reader_parser.pyi
@@ -1,7 +1,7 @@
 from typing import IO, Union
 
-from log_event import LogEvent
-from group_name_resolver import GroupNameResolver
+from .group_name_resolver import GroupNameResolver
+from .log_event import LogEvent
 
 class ReaderParser:
     def __init__(
@@ -9,7 +9,7 @@ class ReaderParser:
         input_stream: IO[bytes],
         schema_content: str,
         group_name_resolver: GroupNameResolver,
-        debug: bool = False
+        debug: bool = False,
     ) -> None: ...
     def parse_next_log_event(self) -> Union[LogEvent, None]: ...
     def reset_input_stream(self, input_stream: IO[bytes]) -> bool: ...
